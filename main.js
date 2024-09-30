133569b67b2fa8ab9781ad682fddcfb82d7978c1
Juuso Määttä <juusomaatta@hotmail.fi>
2024-09-30 18:50:08 +0300

    Add ability to play multiple rounds
    Add playGame function to handle multiple rounds
    Add validateChoice to validate human choice for each round

--- main.js
+++ main.js
@@ -12,53 +12,93 @@ function getComputerChoice(){
     }
 }
 
-// console.log(getComputerChoice());
-
 function getHumanChoice(){
     let choice = prompt(`Choose: "Rock", "Paper", or "Scissors"`)
 
-    return choice;
+    return choice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function validateChoice(choice)
+{
+    if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+        return false;
+    }
 
+    return true;
+}
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
 
-    let result = 0;
+    function playRound(humanChoice, computerChoice){
+        let result = 0;
+    
+        if(humanChoice === computerChoice)
+            result = 0;
+        else if(humanChoice === "rock" && computerChoice === "paper" ||
+                humanChoice === "paper" && computerChoice === "scissors" ||
+                humanChoice === "scissors" && computerChoice === "rock"
+        )
+            result = -1;
+        else if(computerChoice === "rock" && humanChoice === "paper" ||
+            computerChoice === "paper" && humanChoice === "scissors" ||
+            computerChoice === "scissors" && humanChoice === "rock"
+        )   
+            result = 1;
+        else{
+            console.error(`Incorrect inputs ${humanChoice} and ${computerChoice}`);
+        }
 
-    if(humanChoice === computerChoice)
-        result = 0;
-    else if(humanChoice === "rock" && computerChoice === "paper" ||
-            humanChoice === "paper" && computerChoice === "scissors" ||
-            humanChoice === "scissors" && computerChoice === "rock"
-    )
-        result = -1;
-    else if(computerChoice === "rock" && humanChoice === "paper" ||
-        computerChoice === "paper" && humanChoice === "scissors" ||
-        computerChoice === "scissors" && humanChoice === "rock"
-    )   
-        result = 1;
+        return result;
+    }
+
+    let rounds = 5;
+    let currentRound = 0;
+    while(currentRound < rounds){
+
+        const humanChoice = getHumanChoice();
+        if(!validateChoice(humanChoice)){
+            console.log(`Invalid choice: ${humanChoice}`)
+            continue;
+        }
+
+        const computerChoice = getComputerChoice();
+        
+        let roundResult = playRound(humanChoice, computerChoice);
+        if(roundResult === 0){
+            console.log("Draw!");
+        }
+        else if(roundResult === -1){
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+        }
+        else if(roundResult === 1){
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        }
+        currentRound++;
+
+        console.log(`Game ${currentRound}
+Your score: ${humanScore} 
+PC score: ${computerScore}`)
+    }
+
+    if(humanScore > computerScore){
+        console.log(`You won the game!
+Your score: ${humanScore}
+PC score: ${computerScore}`)
+    }
+    else if(computerScore > humanScore){
+        console.log(`You lost the game!
+Your score: ${humanScore}
+PC score: ${computerScore}`)
+    }
     else{
-        console.error(`Incorrect inputs ${humanChoice} and ${computerChoice}`);
-    }
-
-    if(result === 0){
-        console.log("Draw!");
-    }
-    else if(result === -1){
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
-        computerScore++;
-    }
-    else if(result === 1){
-        console.log(`You Win! ${humanChoice} beats ${computerChoice}!`);
-        humanScore++;
+        console.log(`The game was a draw!
+Your score: ${humanScore}
+PC score: ${computerScore}`)
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
