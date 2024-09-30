53a1ab2dda3deac37d21e6853ad676452b379d5e
Juuso Määttä <juusomaatta@hotmail.fi>
2024-09-30 18:50:19 +0300

    Refactor code for initial UI iteration
    Add buttons and basic styling
    Add button click handlers and apply info text on each round

--- main.js
+++ main.js
@@ -27,78 +27,60 @@ function validateChoice(choice)
     return true;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    let result = 0;
 
-    function playRound(humanChoice, computerChoice){
-        let result = 0;
-    
-        if(humanChoice === computerChoice)
-            result = 0;
-        else if(humanChoice === "rock" && computerChoice === "paper" ||
-                humanChoice === "paper" && computerChoice === "scissors" ||
-                humanChoice === "scissors" && computerChoice === "rock"
-        )
-            result = -1;
-        else if(computerChoice === "rock" && humanChoice === "paper" ||
-            computerChoice === "paper" && humanChoice === "scissors" ||
-            computerChoice === "scissors" && humanChoice === "rock"
-        )   
-            result = 1;
-        else{
-            console.error(`Incorrect inputs ${humanChoice} and ${computerChoice}`);
-        }
-
-        return result;
-    }
-
-    let rounds = 5;
-    let currentRound = 0;
-    while(currentRound < rounds){
-
-        const humanChoice = getHumanChoice();
-        if(!validateChoice(humanChoice)){
-            console.log(`Invalid choice: ${humanChoice}`)
-            continue;
-        }
-
-        const computerChoice = getComputerChoice();
-        
-        let roundResult = playRound(humanChoice, computerChoice);
-        if(roundResult === 0){
-            console.log("Draw!");
-        }
-        else if(roundResult === -1){
-            console.log(`You Lose! ${computerChoice} beats ${humanChoice}!`);
-            computerScore++;
-        }
-        else if(roundResult === 1){
-            console.log(`You Win! ${humanChoice} beats ${computerChoice}!`);
-            humanScore++;
-        }
-        currentRound++;
-
-        console.log(`Game ${currentRound}
-Your score: ${humanScore} 
-PC score: ${computerScore}`)
-    }
-
-    if(humanScore > computerScore){
-        console.log(`You won the game!
-Your score: ${humanScore}
-PC score: ${computerScore}`)
-    }
-    else if(computerScore > humanScore){
-        console.log(`You lost the game!
-Your score: ${humanScore}
-PC score: ${computerScore}`)
-    }
+    if(humanChoice === computerChoice)
+        result = 0;
+    else if(humanChoice === "rock" && computerChoice === "paper" ||
+            humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock"
+    )
+        result = -1;
+    else if(computerChoice === "rock" && humanChoice === "paper" ||
+        computerChoice === "paper" && humanChoice === "scissors" ||
+        computerChoice === "scissors" && humanChoice === "rock"
+    )   
+        result = 1;
     else{
-        console.log(`The game was a draw!
-Your score: ${humanScore}
-PC score: ${computerScore}`)
+        console.error(`Incorrect inputs ${humanChoice} and ${computerChoice}`);
     }
+
+    return result;
 }
 
-playGame();
+
+function onToolSelect(e){
+    let humanChoice;
+    switch(e.target.id){
+        case "button-rock":
+            humanChoice = "rock";
+            break;
+        case "button-paper":
+            humanChoice = "paper";
+            break;
+        case "button-scissors":
+            humanChoice = "scissors";
+            break;
+    }
+
+    const computerChoice = getComputerChoice();
+    const roundResult = playRound(humanChoice, computerChoice);
+    let roundString = "";
+    if(roundResult === 0){
+        roundString = "Draw!";
+    }
+    else if(roundResult === -1){
+        roundString = `You Lose! ${computerChoice} beats ${humanChoice}!`;
+    }
+    else if(roundResult === 1){
+        roundString = `You Win! ${humanChoice} beats ${computerChoice}!`;
+    }
+    textRound.textContent = roundString;
+}
+
+const textRound = document.querySelector("#text-round");
+const playerButtons = document.querySelectorAll("#player-buttons > button");
+playerButtons.forEach((button) => {
+    button.addEventListener("click", onToolSelect);
+});
